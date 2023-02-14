7faafc66c6eaf1cece5cdfb2e3920c080d3d0d01
Usama <usamakla1122@gmail.com>
2023-02-15 02:16:25 +0500

    first commit

---
+++ src/Header/Header.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from 'react'
+import './Header.css'
+import Logo from '../assets/logo.png'
+import Bars from '../assets/bars.png'
+import { Link } from 'react-scroll'
+
+const Header = () => {
+
+  const mobile = window.innerWidth<=768?true :false;
+  const[menuOpened,setMenuOpened]=useState(false)
+  return (
+    <div className="header">
+        <img src={Logo} className='logo'/>
+        {(menuOpened==false&&mobile==true)?(
+          <div
+          style={{
+            background:'var(--appColor)',
+            padding:'0.55rem',
+            borderRadius:'5px',
+          }}
+
+          onClick={()=>setMenuOpened(true)}
+          >
+            <img src={Bars} alt="" style={{width:'1.5rem',height:'1.5rem '}}/>
+          </div>
+      ) : <ul className='header-menu'>
+          <li >
+            <Link
+              onClick={() => setMenuOpened(false)}
+              to='home'
+              span={true}
+              smooth={true}
+            
+            >      Home</Link>
+            
+      </li>
+          <li onClick={() => setMenuOpened(false)}> <Link>Programs</Link></li>
+          <li onClick={() => setMenuOpened(false)} > <Link>      Why Us</Link></li>
+          <li onClick={() => setMenuOpened(false)} > <Link>      Plans</Link></li>
+
+          <li > <Link
+            onClick={() => setMenuOpened(false)}
+            to='testimonials'
+            span={true}
+            smooth={true}>  
+            
+              Testimonials</Link></li>
+      </ul>}
+        
+    </div>
+  )
+}
+
+export default Header
---
+++ src/components/Join/Join.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { useRef } from 'react'
+import './Join.css'
+import emailjs from '@emailjs/browser'
+
+const Join = () => {
+   
+
+  return (
+    <div className="Join" id="join-us">
+        <div className="left-j">
+            <hr/>
+            <div>
+                <span className='stroke-text'>READY TO</span>
+                <span>LEVEL UP</span>
+            </div>
+              <div>
+                  <span>YOUR BODY</span>
+                  <span className='stroke-text'>WITH US?</span>
+              </div>
+        </div>
+        <div className="right-j">
+            <form action='' className="email-container">
+                <input type={'email'} name='user_email' placeholder='Enter your Email Address'/>
+                <button className='btn btn-j'>Join Now</button>                
+                </form>
+        </div>
+    </div>
+  )
+}
+
+export default Join
